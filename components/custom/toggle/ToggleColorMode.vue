a51a01f76eb661cdf8ec16a6323e0befe81d1e06
Emerson Tanuri <emersontanuri1@gmail.com>
2025-01-15 22:06:23 -0300

    Update Color Mode Toggle Layout

---
+++ components/custom/toggle/ToggleColorMode.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Sun, Moon } from 'lucide-vue-next'
+const colorMode = useColorMode()
+
+const toggleTheme = () => {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <ClientOnly>
+    <Button @click="toggleTheme" variant="ghost" size="icon">
+      <Moon v-if="colorMode.preference === 'light'" />
+      <Sun v-else />
+    </Button>
+  </ClientOnly>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <CustomSidebar />
     <SidebarInset>
       <header
-        class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+        class="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
       >
         <div class="flex items-center gap-2 px-4">
           <SidebarTrigger class="-ml-1 text-primary" />
@@ -21,8 +21,10 @@
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
-          <CustomToggleColorMode />
         </div>
+        <CustomToggleContainer>
+          <CustomToggleColorMode />
+        </CustomToggleContainer>
       </header>
       <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
@@ -32,6 +34,6 @@
         </div>
         <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
       </div>
-    </SidebarInset> </SidebarProvider
-  >>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
